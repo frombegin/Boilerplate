beacade792b5f0deb7022d282e4252ed5e2d43e8
frombegin <frombegin@gmail.com>
2015-06-02 21:08:41 +0800

    more settings file.

--- sharing/models.py
+++ sharing/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=120, default="unnamed")
+    description = models.TextField()
--- sharing/views.py
+++ sharing/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from sharing import models
 
 # Create your views here.
 def index(request):
+    for item in models.SharingItem.objects.all():
+        print item.id
+        print item.name
+        print item.description
     return HttpResponse(u'hello sharing!')
